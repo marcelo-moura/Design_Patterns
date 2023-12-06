b2da33a15f51a9f330bda155d37917714b048b8d
Marcelo Henrique <marcelohenriquea.moura@outlook.com>
2023-12-05 21:19:07 -0300

    Implementação de exemplo do Singleton

---
+++ DesignPatterns/DesignPatterns.Singleton/LogSingleton.cs
@@ -0,0 +1,27 @@
+﻿namespace DesignPatterns.Singleton
+{
+    public class LogSingleton
+    {
+        public static LogSingleton Instancia;
+
+        private LogSingleton()
+        {
+        }
+
+        public static LogSingleton GetInstancia
+        {
+            get
+            {
+                if (Instancia is null)
+                    Instancia = new();
+
+                return Instancia;
+            }
+        }
+
+        public void Write(string Message)
+        {
+            Console.WriteLine(Message);
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns.Singleton/Program.cs
@@ -0,0 +1,7 @@
+﻿// GARANTIR QUE UMA CLASSE TENHA SOMENTE UMA INSTÂNCIA
+// E FORNECE UM PONTO GLOBAL DE ACESSO PARA A MESMA
+
+using DesignPatterns.Singleton;
+
+var log = LogSingleton.GetInstancia;
+log.Write("Mensagem de log");
