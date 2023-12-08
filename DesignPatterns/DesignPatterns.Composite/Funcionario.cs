fc9a66d47efd917c0b319417260a361d7531c20c
Marcelo Henrique <marcelohenriquea.moura@outlook.com>
2023-12-07 21:45:34 -0300

    Implementação de exemplo do Composite

---
+++ DesignPatterns/DesignPatterns.Composite/Funcionario.cs
@@ -0,0 +1,29 @@
+﻿namespace DesignPatterns.Composite
+{
+    public abstract class Funcionario
+    {
+        public string Nome { get; set; }
+
+        public Funcionario(string nome)
+        {
+            Nome = nome;
+        }
+
+        public abstract string Operation();
+
+        public virtual Funcionario Get(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Add(Funcionario component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Remove(Funcionario component)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns.Composite/FuncionarioComposite.cs
@@ -0,0 +1,43 @@
+﻿namespace DesignPatterns.Composite
+{
+    public class FuncionarioComposite : Funcionario
+    {
+        protected List<Funcionario> _children = new();
+
+        public FuncionarioComposite(string nome) : base(nome)
+        {
+        }
+
+        public override Funcionario Get(int index)
+        {
+            return _children[index];
+        }
+
+        public override void Add(Funcionario component)
+        {
+            _children.Add(component);
+        }
+
+        public override void Remove(Funcionario component)
+        {
+            _children.Remove(component);
+        }
+
+        public override string Operation()
+        {
+            int i = 0;
+            string result = $"Liderados de {Nome} (";
+
+            foreach (Funcionario component in _children)
+            {
+                result += component.Nome;
+                if(i != _children.Count - 1)
+                    result += ", ";
+
+                i++;
+            }
+
+            return result + ")";
+        }
+    }
+}
