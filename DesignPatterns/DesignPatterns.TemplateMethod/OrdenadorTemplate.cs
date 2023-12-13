8c9118a881f993778e126d7e8a96133d19296c8f
Marcelo Henrique <marcelohenriquea.moura@outlook.com>
2023-12-13 19:27:01 -0300

    Implementação de exemplo do Template Method

---
+++ DesignPatterns/DesignPatterns.TemplateMethod/OrdenadorTemplate.cs
@@ -0,0 +1,26 @@
+﻿namespace DesignPatterns.TemplateMethod
+{
+    public abstract class OrdenadorTemplate
+    {
+        public abstract bool IsPrimeiro(Filme filme1, Filme filme2);
+
+        // Metodo de ordenação bolha (Bubble Sort) => O(N^2)
+        public List<Filme> OrdenarFilme(List<Filme> filmes)
+        {
+            for (int i = 0; i < filmes.Count; i++)
+            {
+                for (int j = i + 1; j < filmes.Count; j++)
+                {
+                    if (!IsPrimeiro(filmes[i], filmes[j]))
+                    {
+                        Filme temp = filmes[j];
+                        filmes[j] = filmes[i];
+                        filmes[i] = temp;
+                    }
+                }
+            }
+
+            return filmes;
+        }
+    }
+}
